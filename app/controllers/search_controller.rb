cf02ea42149272d7cf816a51606cb9ee550ae41c
Mina Melosh <email": null>
2017-06-10 20:33:31 -0700

    update index for speed test with breakdown

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,29 +7,51 @@ class SearchController < ApplicationController
 
   def query
     @start = Time.now
+    @mech_time = 0
+    @noko_time = 0
+    @yelp_api_time = 0
+    @google_api_time = 0
     @potential_clients = []
     @location = params[:location]
     @search = params[:business]
     yelp_key = ENV['YELP_API_KEY']
 
+    yelp_start_time = Time.now
+
     @yelp_businesses = HTTParty.get("https://api.yelp.com/v3/businesses/search?location=#{@location}&term=#{@search}", headers: {"Authorization" => "Bearer #{yelp_key}"})['businesses'][0..10]
     @api_count = 1
+    @yelp_api_time += (Time.now - yelp_start_time)
+
 
     @sugar_bunny = Mechanize.new
 
     @yelp_businesses.each do |business|
       yelp_url = business['url']
+
+      mech_start = Time.now
+
       business_page = @sugar_bunny.get(yelp_url)
+
+      @mech_time += (Time.now - mech_start)
       @api_count += 1
+
       if business_page.css('.biz-website a').last
         client_page = business_page.css('.biz-website a').last.text
         http_url = 'http://' + client_page
         speed_key = ENV['SPEED_API_KEY']
         begin
-        response = HTTParty.get("https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=#{http_url}&strategy=mobile&key=#{speed_key}")
+          google_start = Time.now
+          response = HTTParty.get("https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=#{http_url}&strategy=mobile&key=#{speed_key}")
+
+          @google_api_time += (Time.now - google_start)
           @api_count += 1
+
+          noko_start = Time.now
           doc = Nokogiri::HTML(open(response["id"]))
+          @noko_time += (Time.now - noko_start)
+
           @api_count += 1
+
           has_title = doc.css('title').length > 0
           title_points = has_title ? 15 : 0
           # The business gets 15 points if it has a title
